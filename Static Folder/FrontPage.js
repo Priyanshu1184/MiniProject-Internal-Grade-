bc37bf72c9d3f83acc596cbe0401b7afe139dd3c
Priyanshu Sahoo <94666105+Priyanshu1184@users.noreply.github.com>
2023-02-12 10:59:35 +0530

    Add files via upload

---
+++ Static Folder/FrontPage.js
@@ -0,0 +1,46 @@
+
+
+var show = true;
+
+function Showcheckboxes() {
+    var checkboxes =
+        document.getElementById("checkBoxes");
+        
+
+    if (show) {
+        checkboxes.style.display = "block";
+        show = false;
+    } else {
+        checkboxes.style.display = "none";
+        show = true;
+    }
+}
+
+var show = true;
+ function Showcheckboxe() {
+    var checkboxes =
+        document.getElementById("checkBoxe");
+        
+
+    if (show) {
+        checkboxes.style.display = "block";
+        show = false;
+    } else {
+        checkboxes.style.display = "none";
+        show = true;
+    }
+}
+
+function ShowcheckBoxes() {
+    var checkboxes =
+        document.getElementById("CheckBoxes");
+        
+
+    if (show) {
+        checkboxes.style.display = "block";
+        show = false;
+    } else {
+        checkboxes.style.display = "none";
+        show = true;
+    }
+}
